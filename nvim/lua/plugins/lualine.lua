f4beea63c37dd392b6d5ae5c5c4cbc5a5046581d
Darren Taylor <darren.dev.local@gmail.com>
2024-02-27 12:11:52 -0800

    Show relative filename paths in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,14 +1,27 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
-    config = function()
-        local lualine = require("lualine")
-        lualine.setup({
-            options = {
-                theme = "sherbet"
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local lualine = require("lualine")
+		lualine.setup({
+			options = {
+				theme = "sherbet",
+			},
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                },
+                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" },
+            },
+		})
+	end,
 }
