0ddc721b78a94d6bcf1ccfd45f553ccfe3f4e992
Darren Taylor <darren.dev.local@gmail.com>
2025-01-18 15:41:19 -0800

    fix: Formatting test

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,32 +1,32 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig"
-    },
-    config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "ts_ls",
-                "pyright",
-                "marksman"
-            }
-        })
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"ts_ls",
+				"pyright",
+				"marksman",
+			},
+		})
 
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities
-        })
-        lspconfig.ts_ls.setup({
-            capabilities = capabilities
-        })
-        lspconfig.pyright.setup({
-            capabilities = capabilities
-        })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-    end
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+		})
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+	end,
 }
