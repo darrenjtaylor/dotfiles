fe7ded6f141cd052f28d295b22de87be9edd5aa7
Darren Taylor <darren.dev.local@gmail.com>
2025-01-19 20:22:05 -0800

    fix: Change from using none-ls to conform for linting and formatting

---
+++ .config/nvim/lua/plugins/linting_formatting.lua
@@ -0,0 +1,35 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff_fix", "ruff_format" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				markdown = { "prettierd", "prettier", stop_after_first = true },
+				html = { "prettierd", "prettier", stop_after_first = true },
+				css = { "prettierd", "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
+				yaml = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				lsp_fallback = "fallback",
+				async = false,
+				timeout_ms = 500,
+			},
+			notify_on_error = true,
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>f", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "[F]ormat current buffer" })
+	end,
+}
