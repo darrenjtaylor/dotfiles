606868e01d2277b07cee0c33369154cd5b18d0e8
Darren Taylor <darren.dev.local@gmail.com>
2025-01-20 14:59:49 -0800

    chore: Add descriptions to tmux navigator keymaps

--- .config/nvim/lua/plugins/nvim_tmux_navigator.lua
+++ .config/nvim/lua/plugins/nvim_tmux_navigator.lua
@@ -1,7 +1,7 @@
 return {
-    "christoomey/vim-tmux-navigator",
-    vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>"),
-    vim.keymap.set("n", "C-l", ":TmuxNavigateRightCR>"),
-    vim.keymap.set("n", "C-k", ":TmuxNavigateUpCR>"),
-    vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>"),
+	"christoomey/vim-tmux-navigator",
+	vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>", { desc = "TMUX: Navigate to left pane" }),
+	vim.keymap.set("n", "C-l", ":TmuxNavigateRightCR>", { desc = "TMUX: Navigate to right pane" }),
+	vim.keymap.set("n", "C-k", ":TmuxNavigateUpCR>", { desc = "TMUX: Navigate to pane above" }),
+	vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>", { desc = "TMUX: Navigate to pane below" }),
 }
