7439d21155a9f07fccd2d3aa95a516186e55847f
Darren Taylor <darren.dev.local@gmail.com>
2025-01-20 11:10:33 -0800

    chore: Add comment for default nvim-surround keymaps and aliases

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,8 +1,36 @@
 return {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({})
-    end
+	"kylechui/nvim-surround",
+	version = "*",
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({
+			-- Default keymaps to add, delete and change surrounding quotes,
+			-- brackets and braces.
+			--
+			-- keymaps = {
+			--     insert = "<C-g>s",
+			-- 	   insert_line = "<C-g>S",
+			-- 	   normal = "ys",
+			-- 	   normal_cur = "yss",
+			-- 	   normal_line = "yS",
+			-- 	   normal_cur_line = "ySS",
+			-- 	   visual = "S",
+			-- 	   visual_line = "gS",
+			-- 	   delete = "ds",
+			-- 	   change = "cs",
+			-- 	   change_line = "cS",
+			-- },
+			--
+			-- Default aliases for brackets, braces and quotes.
+			--
+			-- aliases = {
+			--     ["a"] = ">",
+			--     ["b"] = ")",
+			--     ["B"] = "}",
+			--     ["r"] = "]",
+			--     ["q"] = { '"', "'", "`" },
+			--     ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+			-- },
+		})
+	end,
 }
