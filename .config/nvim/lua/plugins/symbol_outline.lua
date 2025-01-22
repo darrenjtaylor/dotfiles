95f78231b1616e17d25982476178cac4a0782d28
Darren Taylor <darren.dev.local@gmail.com>
2025-01-22 11:29:23 -0800

    fix: Add description for symbol outline

--- .config/nvim/lua/plugins/symbol_outline.lua
+++ .config/nvim/lua/plugins/symbol_outline.lua
@@ -4,6 +4,6 @@ return {
 		require("symbols-outline").setup({
 			autofold_depth = 0,
 		})
-		vim.keymap.set({ "n", "v" }, "<leader>ts", "<cmd>SymbolsOutline<CR>")
+		vim.keymap.set({ "n", "v" }, "<leader>ts", "<cmd>SymbolsOutline<CR>", { desc = "[T]oggle [S]ymbol Outline" })
 	end,
 }
