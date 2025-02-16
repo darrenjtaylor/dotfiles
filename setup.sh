98670f90c63d121843f8abde3e6ed4901dace08a
Darren Taylor <darren.dev.local@gmail.com>
2025-02-15 21:21:31 -0800

    feat: Add ability to dry run setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
+# Grab the directory this script is running from
 currdir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-filter="$1"
 
-echo "$currdir -- $filter"
+# Define some defaults for our setup args
+filter=""
+dry_run="0"
 
-# Determine which setup scripts to run
+# Parse our command line args
+while [[ $# > 0 ]]; do
+    if [[ $1 == "--dry" ]]; then
+        dry="1"
+    else
+        filter="$1"
+    fi
+    shift
+done
+
+# Grab all of our install scripts
 cd $currdir
 scripts=$(find ./install -maxdepth 1 -mindepth 1 -executable -type f)
 
+# Run our install scripts unless they don't match the provided filter
 for script in $scripts; do
     if echo "$script" | grep -qv "$filter"; then
         echo "skipping $script"
+        continue
+    fi
+
+    if [[ $dry == "1" ]]; then
+        echo "[DRY] Installing $script"
         continue
     fi
 
