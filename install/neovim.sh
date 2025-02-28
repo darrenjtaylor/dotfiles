0ba23858fb675f2bc0438ae3b86a63df7936cad3
Darren Taylor <darren.dev.local@gmail.com>
2025-02-27 22:29:59 -0800

    feat: Add install script to install neovim from source

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Installing neovim..."
+# Install dependencies before we build from source.
+sudo apt install ninja-build gettext cmake curl build-essential -y
+
+SRC_DIR=$HOME/source/neovim
+
+# Cleanup neovim source if it already exists.
+if [ -d $SRC_DIR ]; then
+    rm -rf $SRC_DIR
+fi
+
+# Fetch the latest source.
+git clone https://github.com/neovim/neovim.git $SRC_DIR
+
+# Install from source.
+pushd $SRC_DIR
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+popd
