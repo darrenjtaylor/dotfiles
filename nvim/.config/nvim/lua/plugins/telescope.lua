a16f752755f4a74592a2e5f18595c28ff2f4b2c6
Darren Taylor <darren.dev.local@gmail.com>
2026-03-08 14:02:37 -0700

    fix: Include hidden files and folders when fuzzy finding files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,11 @@ return {
 	},
 	config = function()
 		require("telescope").setup({
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+			},
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown({}),
