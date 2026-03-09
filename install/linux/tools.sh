8fbd9f0dba5715636900c19e98b6f04075b5c697
Darren Taylor <darren.dev.local@gmail.com>
2026-03-08 23:38:11 -0700

    feat: Install dependencies to build and install python via pyenv

--- install/linux/tools.sh
+++ install/linux/tools.sh
@@ -1,13 +1,45 @@
 #!/usr/bin/env bash
 
+##############################################################################
 # Tools and utilities installed via package manager
-sudo apt install -y \
-    jq \
-    ripgrep \
-    stow \
-    tree
+##############################################################################
 
+
+packages=(
+    # Direct-use tools
+    jq
+    ripgrep
+    stow
+    tree
+    # Dependencies to build and install versions of Python with pyenv
+    # Most of these will likely already be installed, but if we're 
+    # installing pyenv we might as well make sure we can definitely
+    # build and install Python from source.
+    make
+    build-essential
+    libssl-dev
+    zlib1g-dev
+    libbz2-dev
+    libreadline-dev
+    libsqlite3-dev
+    curl
+    git
+    libncursesw5-dev
+    xz-utils
+    tk-dev
+    libxml2-dev
+    libxmlsec1-dev
+    libffi-dev
+    liblzma-dev
+)
+sudo apt update
+sudo apt install -y "${packages[@]}"
+
+
+##############################################################################
 # Tools and utilities installed via source or installers
+##############################################################################
+
 
 # Install fzf
 SRC_DIR=$HOME/source
