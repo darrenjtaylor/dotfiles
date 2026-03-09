5955dc3ad648e08fbc59415e581b30a7a1bc56cf
Darren Taylor <darren.dev.local@gmail.com>
2026-03-08 23:02:32 -0700

    refactor: Condense installation of random tools and utilities

--- install/linux/fzf.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-SRC_DIR=$HOME/source
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/source/fzf
-$HOME/source/fzf/install
---
+++ install/linux/tools.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tools and utilities installed via package manager
+sudo apt install -y \
+    jq \
+    ripgrep \
+    tree
+
+# Tools and utilities installed via source
+SRC_DIR=$HOME/source
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/source/fzf
+$HOME/source/fzf/install
