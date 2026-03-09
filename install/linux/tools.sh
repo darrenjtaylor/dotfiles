ae34fabf55b40bf5aec78a1d61b859bb645fcb06
Darren Taylor <darren.dev.local@gmail.com>
2026-03-08 23:26:32 -0700

    feat: Add pyenv to tools installation

--- install/linux/tools.sh
+++ install/linux/tools.sh
@@ -7,7 +7,12 @@ sudo apt install -y \
     stow \
     tree
 
-# Tools and utilities installed via source
+# Tools and utilities installed via source or installers
+
+# Install fzf
 SRC_DIR=$HOME/source
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/source/fzf
 $HOME/source/fzf/install
+
+# Install pyenv
+curl -fsSL https://pyenv.run | bash
