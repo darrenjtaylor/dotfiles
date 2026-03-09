b185e7849f75924036ed8f77da5dde60b667963d
Darren Taylor <darren.dev.local@gmail.com>
2026-03-08 23:12:16 -0700

    fix: Stow my zsh and zim configs before installing zim

--- install/linux/zsh.sh
+++ install/linux/zsh.sh
@@ -3,8 +3,9 @@
 sudo apt install -y zsh
 chsh -s $(which zsh)
 
+# Symlink zsh and zim configuration files.
+stow -t $HOME zsh
+
 # Install zim to manage zsh configuration.
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 
-# Symlink zsh and zim configuration files.
-stow -t $HOME zsh
